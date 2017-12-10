215dacec218b2b2a9f06342f891b25bb7bc8abf2
ShaToya Jones <shatoyajones@ShaToyas-MacBook-Pro.local>
2017-12-10 18:30:33 -0500

    added contents to index.html <-- still needs more, just a starting point

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.get("/scrape", function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every a href within an article tag, and do the following:
-    $("a href").each(function(i, element) {
+    // Now, we grab every h4 within an article tag, and do the following:
+    $("href h2").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
